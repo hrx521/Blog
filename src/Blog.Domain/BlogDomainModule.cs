7a18c5de35e512011ac63f574217e4ce1f13f63b
rupa <31436282@qq.com>
2020-07-05 14:33:36 +0800

    更改项目引用关系。

--- src/Blog.Domain/BlogDomainModule.cs
+++ src/Blog.Domain/BlogDomainModule.cs
@@ -4,7 +4,7 @@ using Volo.Abp.Modularity;
 
 namespace Blog.Domain
 {
-    [DependsOn(typeof(AbpIdentityDomainModule))]
+    [DependsOn(typeof(AbpIdentityDomainModule),typeof(BlogDomainSharedModule))]
     public class BlogDomainModule : AbpModule
     {
 
